909f4e954be82cb09588aecf3ab79c59d5e7462d
Nicholas Polosky <njp59@cornell.edu>
2020-05-10 14:52:56 -0400

    removed bugs and cleaned code+readme

--- GPBayesianOptimizer.py
+++ GPBayesianOptimizer.py
@@ -1,25 +1,23 @@
 import numpy as np
 import time
 from typing import Callable
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import pyplot as plt
 
 from cmaes import CMAES
 
-def inputFunction(x:float)->float:
-    return 10. + (-1. * x**(4.) * 2.0*np.sin(2.*np.pi*4.*x))
-
 def MorletWavelet(vec:np.ndarray)->float:
     """
-        Returns the value of a Morlet Wavelet evaluated at x
+        Returns the value of a Morlet Wavelet evaluated at vec=(x,y). Specifically,
+        the function below is a Morlet Wavelet in the first dimension and a bump
+        function in the second dimension.
 
-    :param x: the input value at which to evaluate the Morlet Wavelet Function
+    :param vec: the input value at which to evaluate the Morlet Wavelet Function
     """
     x = vec[0]
     y = vec[1]
-    return ((1./np.sqrt(2.*np.pi*4.0))*np.exp(-x**2./4.0) * 2.0*np.sin(2.*np.pi*0.25*x)) * (1./np.sqrt(2.*np.pi*4.0))*np.exp(-y**2./4.0)
+    return ((1./np.sqrt(0.5*np.pi))*np.exp(-x**2./4.0) * 2.0*np.sin(2.*np.pi*0.3*x)) * (1./np.sqrt(0.1*np.pi))*np.exp(-y**2./0.1)
 
-def bayesOpt(inputs:np.ndarray, outputs:np.ndarray, func:Callable, theta_d:np.ndarray, maximize:bool=True):
+def bayesOpt(inputs:np.ndarray, outputs:np.ndarray, func:Callable, theta_d:np.ndarray, num_iters:int,
+             maximize:bool=True, chol:bool =False):
     """
         Function which performs bayesian optimization using Gaussian Processes and
         the CMA-ES optimization algorithm.
@@ -28,113 +26,94 @@ def bayesOpt(inputs:np.ndarray, outputs:np.ndarray, func:Callable, theta_d:np.nd
     :param outputs: the values of the objective function evaluated at each input value.
     :param func: the objective function.
     :param theta_d: hyperparameter for the Gaussian Process.
+    :param num_iters: number of GP iteration to run.
     :param maximize: indicate whether the objective function should be maximized (True) or minimized (False).
+    :param chol: indicate whether to use Cholesky decomposition for GP evaluation (True) or to use
+                 matrix inversion (False).
     """
     if len(inputs.shape) < 2: inputs = np.expand_dims(inputs, -1)
     if maximize: UCB = -np.inf 
     else: UCB = np.inf
-    # maxRet = np.max(outputs)
     
     x_t = np.zeros(inputs.shape[1])
     t = 0
     bestx_t = x_t
     bestUCB = UCB
     #GP iterations
-    while t<10:
+    while t<num_iters:
         #this function computes the upper confidence value of the GP at x_t
-        def kernelFunc(x_t:np.ndarray):
-            theta_0 = 0.5
+        def kernelFunc(x_t:np.ndarray, maximize:bool, chol:bool):
+            theta_0 = 0.1
             kappa = 0.1
 
             kernel = np.hstack([inputs.reshape(inputs.shape[0],1,inputs.shape[1])] * inputs.shape[0])
-            # print(kernel.shape)
             kernelT = np.transpose(kernel, (1,0,2))
-            # print(kernelT.shape)
+            
             #RBF kernel for covariance matrix
             K = theta_0 * np.exp(-0.5 * np.sum(np.power(kernel - kernelT, 2.) / np.power(theta_d, 2.), axis=-1))
-            fake_K = theta_0 * np.fromfunction(lambda i, j: np.exp( -0.5 * np.sum(np.power(inputs[i.astype(np.int),:] - inputs[j.astype(np.int),:], 2.) / np.power(theta_d, 2.), axis=-1)),
-                                    (25,25), dtype=inputs.dtype)
-            # print("fake_K: {}".format(fake_K))
-            # print("K: {}".format(K))
-            # print("equal?: {}".format(np.all(K==fake_K)))
+            # K = theta_0 * np.fromfunction(lambda i, j: np.exp( -0.5 * np.sum(np.power(inputs[i.astype(np.int),:] - inputs[j.astype(np.int),:], 2.) / np.power(theta_d, 2.), axis=-1)),
+            #                         (25,25), dtype=inputs.dtype)
 
-            # print("x_t: {}".format(x_t))
             K_t = theta_0 * np.exp(-0.5 * np.sum(np.power(inputs - x_t, 2.) / np.power(theta_d, 2.), axis=-1))
-            # print("K_t: {}".format(K_t))
+            K_tt = theta_0 * (np.exp(-0.5 * np.sum(np.power(x_t - x_t, 2.) / np.power(theta_d, 2.), axis=-1)) + np.random.normal(0,1e-5))
             
-            K_tt = theta_0 * (np.exp(-0.5 * np.sum(np.power(x_t - x_t, 2.) / np.power(theta_d, 2.), axis=-1)))# + np.random.normal(0,0.01))
-            KI_inv = np.linalg.inv(K + 1e-3*np.eye(inputs.shape[0]))
-            mean = np.dot(K_t.transpose(), np.dot(KI_inv, outputs))
-            var = K_tt + 1e-3 - np.dot(K_t.transpose(), np.dot(KI_inv, K_t))
-            if var < 0:
-                print("K_tt: {}".format(K_tt))
-                print("mean: {}".format(mean))
-                print("var: {}".format(var))
-                exit()
-            UCB = mean + kappa*np.sqrt(var)
+            if chol:
+                L = np.linalg.cholesky(K + 1e-3*np.eye(inputs.shape[0]))
+                alpha = np.linalg.solve(L.T, np.linalg.solve(L,outputs))
+                mean = K_t.dot(alpha)
+                v = np.linalg.solve(L,K_t)
+                var = K_tt - v.dot(v)
+            else:
+                KI_inv = np.linalg.inv(K + 1e-3*np.eye(inputs.shape[0]))
+                mean = np.dot(K_t.transpose(), np.dot(KI_inv, outputs))
+                var = K_tt - np.dot(K_t.transpose(), np.dot(KI_inv, K_t))
+
+            assert var > 0, "Var < 0, please reduce K_tt noise"
+
+            if maximize: UCB = mean + kappa*np.sqrt(var)
+            else: UCB = mean - kappa*np.sqrt(var)
             return UCB
 
         #run CMA-ES to find maximum of the GP
-        bestX = CMAES(inputs[np.argmax(outputs)], 0.1, 100, kernelFunc, None, False)
-        # es = cma.CMAEvolutionStrategy(inputs[np.argmax(outputs)], 0.1)
-        # for _ in range(0, 1000):
-        #     new_xs = es.ask()
-        #     # print(new_xs)
-        #     new_outputs = [kernelFunc(x) for x in new_xs]
-        #     # print(new_outputs)
-        #     if maximize: new_outputs = [-x for x in new_outputs]
-        #     es.tell(new_xs, new_outputs)
+        if maximize: xmean = inputs[np.argmax(outputs)]
+        else: xmean = inputs[np.argmin(outputs)]
+
+        bestX = CMAES(xmean, 0.1, 100, 
+                      lambda x: kernelFunc(x, maximize, chol), None, not maximize)
 
         inputs = np.vstack((inputs, bestX))
         outputs = np.hstack((outputs, func(bestX)))
+        if maximize:
+            bestx_t = inputs[np.argmax(outputs)]
+            print("Current best x_t: {}, best val: {}".format(bestx_t, np.max(outputs)))
+        else:
+            bestx_t = inputs[np.argmin(outputs)]
+            print("Current best x_t: {}, best val: {}".format(bestx_t, np.min(outputs)))
         t+=1
     return inputs, outputs
 
 
 def main():
-    inputsx = np.arange(-5.0, 5.0, 2.2)
-    inputsy = np.arange(-5.0, 5.0, 2.2)
+    inputsx = np.arange(-5.0, 5.0, 1.8)
+    inputsy = np.arange(-5.0, 5.0, 1.8)
+    
     xx,yy = np.meshgrid(inputsx, inputsy)
     outputs = np.array([MorletWavelet(np.array([x, y])) for (x,y) in zip(xx,yy)])
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # ax.contour(inputsx, inputsy, outputs)
-    # ax.set(xlim=(-5.5, 5.5), ylim=(-5.5, 5.5), zlim=(-0.5, 0.5))
-    # ax.set_xlabel("Input")
-    # ax.set_zlabel("Output")
-    # ax.set_title("Initial Samples")
-    # plt.show()
 
-    #num samples x num parameters
-    print(xx.shape)
-    print(np.ravel(yy).shape)
-    print(np.ravel(outputs).shape)
     inputs = np.vstack((np.ravel(xx), np.ravel(yy))).transpose()
-    xs, ys = bayesOpt(inputs, np.ravel(outputs), MorletWavelet, np.array([10., 10.]))
-    print(xs)
-    print(ys)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.contour(xs[:0], xs[:1], ys)
-    ax.set(xlim=(-5.5, 5.5), ylim=(-5.5, 5.5))
-    ax.set_xlabel("Input")
-    ax.set_ylabel("Output")
-    ax.set_title("Gaussian Processs")
-    plt.show()    
+    length = np.array([0.1, 2.])
+    num_iters = 10
+    maximize = True
+    xs, ys = bayesOpt(inputs, np.ravel(outputs), MorletWavelet, length, num_iters, maximize=maximize)
 
-    xVals = np.arange(-5.0, 5.0, 0.01)
-    yVals = np.arange(-5.0, 5.0, 0.01)
-    xx, yy = np.meshgrid(xVals, yVals)
-    outs = np.array([MorletWavelet(np.ravel(xx), np.ravel(yy))])
-    outs = outs.reshape(xx.shape)
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.contour(xVals, yVals, outs)
-    ax.set(xlim=(-5.5, 5.5), ylim=(-5.5, 5.5))
-    ax.set_xlabel("Input")
-    ax.set_zlabel("Output")
-    ax.set_title("Ground Truth")
-    plt.show()
+    if maximize:
+        print("max of inputs: {}".format(np.max(outputs)))
+        print("max of Bopt: {}".format(np.max(ys)))
+        print("Improvement: {}".format(abs(np.max(outputs) - np.max(ys))))
+    else:
+        print("min of inputs: {}".format(np.min(outputs)))
+        print("min of Bopt: {}".format(np.min(ys)))
+        print("Improvement: {}".format(abs(np.min(outputs) - np.min(ys))))
 
 if __name__ == '__main__':
     main()
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # BayesianOptimization
 
-This repository contains code which optimizes a function using Gaussian Process Regression. The code currently depends on the pycma module as it uses the CMA-ES optimization algorithm to compute the next exploration point in the Gaussian Process.  
+This repository contains code which optimizes a function using Gaussian Process Regression. The code uses the CMA-ES optimization algorithm to compute the next exploration point in the Gaussian Process.  
 
-The example shows the Bayesian Optimization algorithm performing Gaussian Process Regression on a Morlet Wavelet Function.
+The example shows the Bayesian Optimization algorithm performing Gaussian Process Regression on a function which is a Morlet Wavelet in the first dimension and a simple bump function in the second dimension.
